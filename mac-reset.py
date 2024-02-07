83981c8c61dc9586e8ea13ca3dca1f414674f4a7
Aryan Sharma <inbox.aryan@gmail.com>
2024-02-06 21:32:10 -0500

    Added mac-reset and updated mac-changer

--- mac-reset.py
+++ mac-reset.py
@@ -3,5 +3,6 @@
 import subprocess
 
 subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55", shell=True)
+subprocess.call("ifconfig eth0 hw ether $(ethtool -P eth0 | awk '{print $3}')", shell=True)
 subprocess.call("ifconfig eth0 up", shell=True)
+subprocess.call("ifconfig eth0", shell=True)
